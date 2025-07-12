fe2531c9b2ac69c9bc6494fcaa9f7592c1738562
Conrad Ludgate <conradludgate@gmail.com>
2025-07-12 10:58:03 +0100

    fix blocking loops

--- src/monitoring/virtual_memory.rs
+++ src/monitoring/virtual_memory.rs
@@ -9,6 +9,10 @@ pub async fn virtual_memory(pid: Pid) -> (u64, f64, u64) {
     let mut min: u64 = u64::MAX;
 
     loop {
+        // prevent this loop from blocking,
+        // since this loop only exits if the process terminates
+        tokio::task::yield_now().await;
+
         sys.refresh_processes(ProcessesToUpdate::Some(&[pid]), true);
 
         if let Some(process) = sys.process(pid) {
